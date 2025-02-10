39098afc76509861fd24d27bf2f1bd97fe12f996
inferno0230 <mail@inferno0230.in>
2025-02-10 05:26:42 +0000

    aston: Enable OplusTouch soong config
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -48,6 +48,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Touch
+$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,USE_OPLUSTOUCH,true)
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator.service.oplus-richtap
